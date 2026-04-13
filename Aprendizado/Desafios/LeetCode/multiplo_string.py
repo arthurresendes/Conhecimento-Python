2f7869c363457aa3cbe22976f787ff97f5f80ab9
Arthur Resende Gomes <arthur.resende.gomes02@gmail.com>
2026-04-13 15:52:23 -0300

    Atualizar o multiplo_string.py

--- Aprendizado/Desafios/LeetCode/multiplo_string.py
+++ Aprendizado/Desafios/LeetCode/multiplo_string.py
@@ -1,10 +1,7 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1 = int(num1)
-        num2 = int(num2)
-        res = num1 * num2
-        return str(res)
-
+        res = int(num1) * int(num2)
+        return f' "{str(res)}" '
 
 
 if __name__ == "__main__":
